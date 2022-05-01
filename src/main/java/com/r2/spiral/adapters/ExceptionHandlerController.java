8887eccf904f35180bb089379756160375787997
RodrigoNoblega <rodrigo.noblega@mercadolibre.com>
2022-05-01 13:03:44 -0300

    Unused import

--- src/main/java/com/r2/spiral/adapters/ExceptionHandlerController.java
+++ src/main/java/com/r2/spiral/adapters/ExceptionHandlerController.java
@@ -2,7 +2,6 @@ package com.r2.spiral.adapters;
 
 import com.r2.spiral.adapters.output.ErrorResponse;
 import com.r2.spiral.adapters.request.validations.RequestValidationException;
-import com.r2.spiral.configuration.Instrumentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
